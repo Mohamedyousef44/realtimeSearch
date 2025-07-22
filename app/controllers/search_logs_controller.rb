50aaa2deac94ea55c4d0a4dec925e17adb04288d
mohamed youssef <mohamed.youssef@objects.com>
2025-07-22 20:31:04 +0300

    create function to set the user before the request

--- app/controllers/search_logs_controller.rb
+++ app/controllers/search_logs_controller.rb
@@ -1,5 +1,6 @@
 class SearchLogsController < ApplicationController
-  
+    before_action :set_user
+    
     # POST /search_logs
     def create
         if params[:query].present? && params[:query].length > 2 
@@ -11,6 +12,12 @@ class SearchLogsController < ApplicationController
     # GET /search_logs
     def index
     
+    end
+
+    private
+
+    def set_user
+        @user = User.find_or_create_by(ip: request.remote_ip)
     end
   
   end
