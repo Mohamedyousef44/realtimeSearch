8a8bbe16fcac7807ef918958e65fa64e5f0a3acc
mohamed youssef <mohamed.youssef@objects.com>
2025-07-22 20:41:23 +0300

    fix issue of csrf authentication error

--- app/controllers/search_logs_controller.rb
+++ app/controllers/search_logs_controller.rb
@@ -1,23 +1,22 @@
 class SearchLogsController < ApplicationController
-    before_action :set_user
-    
-    # POST /search_logs
-    def create
-        if params[:query].present? && params[:query].length > 2 
-          @user.search_logs.create!(query: params[:query])
-        end
-        head :ok
-      end
-  
-    # GET /search_logs
-    def index
-    
-    end
+  skip_before_action :verify_authenticity_token
+  before_action :set_user
 
-    private
-
-    def set_user
-        @user = User.find_or_create_by(ip: request.remote_ip)
+  # POST /search_logs
+  def create
+    if params[:query].present? && params[:query].length > 2
+      @user.search_logs.create!(query: params[:query])
     end
-  
+    head :ok
   end
+
+  # GET /search_logs
+  def index
+  end
+
+  private
+
+  def set_user
+    @user = User.find_or_create_by(ip: request.remote_ip)
+  end
+end
