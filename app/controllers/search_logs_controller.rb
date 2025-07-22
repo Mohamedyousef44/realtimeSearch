93090f98c1ff6253e294c45859f91c64b77ba615
mohamed youssef <mohamed.youssef@objects.com>
2025-07-22 20:26:03 +0300

    create controller function  to create query for a user

--- app/controllers/search_logs_controller.rb
+++ app/controllers/search_logs_controller.rb
@@ -2,8 +2,11 @@ class SearchLogsController < ApplicationController
   
     # POST /search_logs
     def create
-
-    end
+        if params[:query].present? && params[:query].length > 2 
+          @user.search_logs.create!(query: params[:query])
+        end
+        head :ok
+      end
   
     # GET /search_logs
     def index
