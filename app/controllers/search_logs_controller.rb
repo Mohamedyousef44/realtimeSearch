ee185c5355005daa09968818de4e206433d7db78
mohamed youssef <mohamed.youssef@objects.com>
2025-07-24 23:39:51 +0300

    add some logs

--- app/controllers/search_logs_controller.rb
+++ app/controllers/search_logs_controller.rb
@@ -3,6 +3,8 @@ class SearchLogsController < ApplicationController
   
     # POST /search_logs
     def create
+      Rails.logger.info "Create query for IP is #{ip}"
+
       session_id = params[:session_id].to_s.strip
       query = params[:query].to_s.strip.downcase
       ip = request.remote_ip
@@ -25,6 +27,7 @@ class SearchLogsController < ApplicationController
     def index
       ip = request.remote_ip
   
+      Rails.logger.info "ip is:  #{ip}"
       analytics = SearchLog
                     .where(ip_address: ip)
                     .group(:query)
